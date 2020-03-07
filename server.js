cc6203c5c8c759f565e08ede5773ebacaccc9d3e
Max Lewis <max.a.lewis98@gmail.com>
2020-03-06 17:09:14 -0700

    basic html file .get functions

---
+++ server.js
@@ -0,0 +1,25 @@
+// 2. Your `server.js` file should require the basic npm packages we've used in class: `express` and `path`.
+
+// ==============================================================================
+// DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
+// ==============================================================================
+
+var express = require("express");
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// require("./app/routing/apiRoute")(app);
+require("./app/routing/htmlRoute")(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
+  
